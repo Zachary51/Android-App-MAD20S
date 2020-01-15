84c00457512a7a451be962c9396e6b7d42e403d7
Zachary51 <zaricwong51@gmail.com>
2020-01-14 22:12:52 -0800

    assignment1 codes

---
+++ app/src/test/java/com/mycompany/numad20s_zeyiwang/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.mycompany.numad20s_zeyiwang;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
